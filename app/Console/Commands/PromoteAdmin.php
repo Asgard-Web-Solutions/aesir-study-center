15e2b36c8be760e2e23ec7f095bd085e82ce8910
Jon Zenor <JonZenor@gmail.com>
2020-01-29 20:54:24 -0700

    Got user stuff working, including a make admin command. Also Tailwind.

---
+++ app/Console/Commands/PromoteAdmin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Role;
+use App\User;
+use Illuminate\Console\Command;
+
+class PromoteAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'promote:admin {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Promote a user to an admin.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $userEmail = $this->argument('email');
+
+        $user = User::where('email', '=', $userEmail)->first();
+        $role = Role::where('name', '=', 'admin')->first();
+
+        $user->roles()->attach($role);
+    }
+}
