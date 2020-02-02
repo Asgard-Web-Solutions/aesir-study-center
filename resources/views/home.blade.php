282deb3b116e84667420bce5bbe7f78950814831
Jon Zenor <JonZenor@gmail.com>
2020-02-01 23:49:13 -0700

    Closes #22 Redesigned home page for mobile. It has a different view, but it looks better. Things bo gack to normal on sm.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class=" items-center">
             
-        <div class="w-full sm:w-10/12 md:w-8/12 m-auto bg-gray-200 rounded-lg">
+        <div class="w-full sm:w-10/12 md:w-8/12 m-auto bg-gray-200 rounded-lg hidden sm:block">
             <div class="w-full bg-gray-700 rounded-t-lg text-center">
                 <h1 class="text-white text-2xl">Your Test History</h1>
             </div>
@@ -35,6 +35,79 @@
 
             </div>
         </div>
+
+        <div class="w-full sm:hidden">
+            @foreach ($sets as $set)
+                <div class="w-full sm:w-10/12 md:w-8/12 m-auto bg-gray-200 rounded-lg my-5">
+                    <div class="w-full bg-gray-700 rounded-t-lg text-center">
+                        <h1 class="text-white text-2xl">{{ $set['name'] }}</h1>
+                    </div>
+
+                    <div class="w-full my-2 text-xl">
+
+                        <div class="flex">
+                            <div class="w-5/12">
+                                <strong>Avg Grade Last {{ config('test.count_tests_for_average_score') }}</strong>
+                            </div>
+                            <div class="w-4/12">
+                                {{ $set['average'] }}%
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="w-5/12">
+                                <strong>Times Taken</strong>
+                            </div>
+                            <div class="w-4/12">
+                                {{ $set['taken'] }}
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="w-5/12">
+                                <strong>Since Last Test</strong>
+                            </div>
+                            <div class="w-4/12">
+                                {{ $set['last_time'] }}
+                            </div>
+                        </div>
+                        <br />
+
+                        <div class="flex">
+                            <div class="w-5/12">
+                                <strong>Familiar</strong>
+                            </div>
+                            <div class="w-4/12">
+                                {{ $set['familiar'] }}%
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="w-5/12">
+                                <strong>Proficiency</strong>
+                            </div>
+                            <div class="w-4/12">
+                                {{ $set['proficient'] }}%
+                            </div>
+                        </div>
+
+                        <div class="flex">
+                            <div class="w-5/12">
+                                <strong>Mastery</strong>
+                            </div>
+                            <div class="w-4/12">
+                                {{ $set['mastery'] }}%
+                            </div>
+                        </div>
+
+                        <div class="text-right text-base p-4">
+                            <a href="{{ route('test-history', $set['id']) }}" class="px-3 bg-gray-800 rounded-lg text-white">Exam History</a>
+                        </div>
+
+                    </div>
+                </div>
+            @endforeach
+        </div>
         
         @if ($incomplete->count())
             <div class="w-full sm:w-10/12 md:w-8/12 m-auto bg-gray-200 rounded-lg mt-5">
@@ -52,8 +125,9 @@
             </div>
         @endif
 
-        <div class="w-full sm:w-10/12 md:w-8/12 m-auto rounded-lg mt-5 text-right">
-            <a href="{{ route('tests') }}" class="px-3 bg-gray-800 rounded-lg text-white">Take A Test</a>
+        <div class="w-full sm:w-10/12 md:w-8/12 m-auto rounded-lg mt-5 text-center sm:text-right">
+            <a href="{{ route('tests') }}" class="px-3 bg-gray-800 rounded-lg text-white text-lg sm:text-base">Take A Test</a>
         </div>
     </div>
+
 @endsection
