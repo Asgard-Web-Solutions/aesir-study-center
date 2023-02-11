abc05479818fc78c47fbcbb5f37f8232fb40da30
Shift <shift@laravelshift.com>
2023-02-11 19:31:10 +0000

    Adopt class based routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,30 +18,30 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->group(function () {
     Voyager::routes();
 });
 
-Route::get('/exams', 'TestController@sets')->name('tests')->middleware('auth');
-Route::get('/exam/{id}', 'TestController@select')->name('select-test')->middleware('auth');
-Route::post('/exam/{id}/start', 'TestController@start')->name('start-test')->middleware('auth');
-Route::get('/test/{id}', 'TestController@test')->name('take-test')->middleware('auth');
-Route::post('/test/{id}', 'TestController@answer')->name('answer')->middleware('auth');
+Route::get('/exams', [TestController::class, 'sets'])->name('tests')->middleware('auth');
+Route::get('/exam/{id}', [TestController::class, 'select'])->name('select-test')->middleware('auth');
+Route::post('/exam/{id}/start', [TestController::class, 'start'])->name('start-test')->middleware('auth');
+Route::get('/test/{id}', [TestController::class, 'test'])->name('take-test')->middleware('auth');
+Route::post('/test/{id}', [TestController::class, 'answer'])->name('answer')->middleware('auth');
 
-Route::get('/history/{id}', 'HomeController@history')->name('test-history')->middleware('auth');
+Route::get('/history/{id}', [HomeController::class, 'history'])->name('test-history')->middleware('auth');
 
-Route::get('/manage', 'QuestionController@exams')->name('manage-exams')->middleware('auth');
-Route::get('/manage/{id}', 'QuestionController@index')->name('manage-questions')->middleware('auth');
-Route::get('/manage/{id}/add', 'QuestionController@add')->name('add-question')->middleware('auth');
-Route::post('/manage/{id}/add', 'QuestionController@store')->name('save-question')->middleware('auth');
+Route::get('/manage', [QuestionController::class, 'exams'])->name('manage-exams')->middleware('auth');
+Route::get('/manage/{id}', [QuestionController::class, 'index'])->name('manage-questions')->middleware('auth');
+Route::get('/manage/{id}/add', [QuestionController::class, 'add'])->name('add-question')->middleware('auth');
+Route::post('/manage/{id}/add', [QuestionController::class, 'store'])->name('save-question')->middleware('auth');
 
-Route::get('/question/{id}', 'QuestionController@answers')->name('manage-answers')->middleware('auth');
-Route::post('/question/{id}/add', 'QuestionController@storeAnswer')->name('save-answers')->middleware('auth');
-Route::get('/answer/{id}/edit', 'QuestionController@editAnswer')->name('edit-answer')->middleware('auth');
-Route::post('/answer/{id}/edit', 'QuestionController@updateAnswer')->name('update-answer')->middleware('auth');
-Route::get('/answer/{id}/delete', 'QuestionController@deleteAnswer')->name('delete-answer')->middleware('auth');
-Route::post('/answer/{id}/delete', 'QuestionController@deleteAnswerConfirm')->name('delete-answer-confirm')->middleware('auth');
+Route::get('/question/{id}', [QuestionController::class, 'answers'])->name('manage-answers')->middleware('auth');
+Route::post('/question/{id}/add', [QuestionController::class, 'storeAnswer'])->name('save-answers')->middleware('auth');
+Route::get('/answer/{id}/edit', [QuestionController::class, 'editAnswer'])->name('edit-answer')->middleware('auth');
+Route::post('/answer/{id}/edit', [QuestionController::class, 'updateAnswer'])->name('update-answer')->middleware('auth');
+Route::get('/answer/{id}/delete', [QuestionController::class, 'deleteAnswer'])->name('delete-answer')->middleware('auth');
+Route::post('/answer/{id}/delete', [QuestionController::class, 'deleteAnswerConfirm'])->name('delete-answer-confirm')->middleware('auth');
 
-Route::post('/exam/add', 'QuestionController@storeExam')->name('save-exam')->middleware('auth');
+Route::post('/exam/add', [QuestionController::class, 'storeExam'])->name('save-exam')->middleware('auth');
