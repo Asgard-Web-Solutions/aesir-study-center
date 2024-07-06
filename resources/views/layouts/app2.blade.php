58eeb151f31fae40b351198b3890e4d5bee859ef
Jon Zenor <JonZenor@gmail.com>
2024-07-06 01:27:49 -0600

    Fixed the mobile menu I hope

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -42,10 +42,47 @@
                         
                     @endguest
                 </div>
-                <div class="dropdown dropdown-end md:hidden">
+                <div class="dropdown md:hidden">
+                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-5 h-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h7" />
+                      </svg>
+                    </div>
+                    <ul
+                      tabindex="0"
+                      class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <x-button.mobile href="{{ route('tests') }}">Exam List</x-button.mobile>
+                        @auth
+                            @if ( auth()->user()->hasRole('admin') )
+                                <x-button.mobile href="{{ route('manage-exams') }}">Manage Exams</x-button.mobile>
+                            @endif
+                        @endauth
+
+                        <hr />
+                        @auth
+                            <x-button.mobile href="{{ route('home') }}">Home</x-button.mobile>
+
+                            <a href="{{ route('logout') }}" class="block px-4 py-2" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                {{ csrf_field() }}
+                            </form>
+                        @endauth
+                    </ul>
+                  </div>
+              
+                {{-- <div class="dropdown dropdown-end md:hidden">
                     <button id="menu-button" class="btn btn-ghost">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /> MENU
                         </svg>
                     </button>
                     <div tabindex="0" class="p-2 shadow dropdown-content menu bg-base-100 rounded-box w-52">
@@ -66,7 +103,7 @@
                             </form>
                         @endauth
                     </div>
-                </div>
+                </div> --}}
 
             </div>
         </nav>
