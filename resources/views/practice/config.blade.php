5151da8ce7cb4509a0b35798a1edf2961d1819bd
Jon Zenor <JonZenor@gmail.com>
2024-07-14 13:50:54 -0600

    Started creating the Practice Config page

---
+++ resources/views/practice/config.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app2')
+
+@section('content')
+
+    <x-card.main title="Configure your practice settings">
+        <x-card.mini>
+            <form action="{{ route('practice-config-update', $set) }}" method="post">
+                @csrf
+
+                <x-form.text label="Name" name="name" />
+                <x-form.text label="Description" name="description" />
+
+                <x-card.buttons submitLabel="Update your configuation" />
+            </form>
+        </x-card.mini>
+    </x-card.main>
+
+@endsection
