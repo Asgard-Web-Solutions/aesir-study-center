e95e49b40c0428ba843bce225e703b2920a1ff59
Jon Zenor <JonZenor@gmail.com>
2024-07-17 23:57:30 -0600

    Greatly enhanced the look of the summary details

--- resources/views/exam-session/summary.blade.php
+++ resources/views/exam-session/summary.blade.php
@@ -2,19 +2,46 @@
 
 @section('content')
 
-    <x-card.main title="Exam Summary: {{ $examSet->name }}">
+    <x-card.main title="{{ $examSet->name }} - Summary">
         <x-card.mini>
             <div class="w-1/2 mx-auto shadow stats">
                 <div class="stat">
                   <div class="stat-title text-accent">Grade</div>
                   <div class="stat-value text-primary">{{ $session->grade }}%</div>
-                  <div class="stat-desc">You got {{ $session->correct_answers }} out of {{ $session->question_count }} questions correct.</div>
+                  <div class="stat-desc">Completed {{ $session->date_completed }}</div>
                 </div>
               </div>
-              
-              {{-- // TODO: Make this look better --}}
-            <x-text.main label="Correct">{{ $session->correct_answers }}</x-text.main>
-            <x-text.main label="Incorrect">{{ $session->incorrect_answers }}</x-text.main>
+        </x-card.mini>
+
+        <x-card.mini title="Answer Statistics">
+              <div class="shadow stats">
+                <div class="stat">
+                  <div class="stat-figure text-success">
+                    <i class="fa-solid fa-file-check"></i>
+                  </div>
+                  <div class="stat-title">Correct</div>
+                  <div class="stat-value text-success">{{ $session->correct_answers }}</div>
+                  <div class="stat-desc"></div>
+                </div>
+
+                <div class="stat">
+                  <div class="stat-figure text-error">
+                    <i class="fa-solid fa-file-excel"></i>
+                  </div>
+                  <div class="stat-title">Incorrect</div>
+                  <div class="stat-value text-error">{{ $session->incorrect_answers }}</div>
+                  <div class="stat-desc"></div>
+                </div>
+
+                <div class="stat">
+                  <div class="stat-figure text-info">
+                    <i class="fa-duotone fa-solid fa-files"></i>
+                  </div>
+                  <div class="stat-title">Total Questions</div>
+                  <div class="stat-value text-info">{{ $session->question_count }}</div>
+                  <div class="stat-desc"></div>
+                </div>
+              </div>
 
             {{-- // TODO: Show mastery stats --}}
             {{-- // TODO: Button to take the test again // Button to return to dashboard --}}
