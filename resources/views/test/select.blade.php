5dad3235405e0ac7e1fdd62ad2f7ffec4eaa44b0
Jon Zenor <JonZenor@gmail.com>
2024-07-20 17:07:07 -0600

    Updated links on public page to start new tests

--- resources/views/test/select.blade.php
+++ resources/views/test/select.blade.php
@@ -12,7 +12,7 @@
                     @if ($set->user )<div class="block m-1 md:flex badge badge-secondary">Author: {{ $set->user->name }}</div>@endif
                 </div>
                 <div class="justify-end w-full text-right card-action">
-                    <a href="{{ route('select-test', $set->id) }}" class="btn btn-primary">{{ __('TAKE TEST') }}</a>
+                    <a href="{{ route('exam-session.start', $set) }}" class="btn btn-primary">{{ __('TAKE TEST') }}</a>
                 </div>
             </x-card.mini>
         @endforeach
@@ -25,7 +25,7 @@
                     <x-text.dim>{{ $set->description }}</x-text.dim>
                     <x-text.dim label='Question Pool:'>{{ $set->questions->count() }}</x-text.dim>
                     <div class="justify-end w-full text-right card-action">
-                        <a href="{{ route('select-test', $set->id) }}" class="btn btn-primary">{{ __('TAKE TEST') }}</a>
+                        <a href="{{ route('exam-session.start', $set) }}" class="btn btn-primary">{{ __('TAKE TEST') }}</a>
                     </div>
                 </x-card.mini>
             @endforeach
