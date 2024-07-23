c87f2534ef596d145ed68a45ccf00660bb30b902
Jon Zenor <JonZenor@gmail.com>
2024-07-23 01:07:54 -0600

    Enabled the flash cards feature

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Feature::define('flash-cards', function() {
+            return true;
+            
             if (app()->environment(['local', 'testing'])) {
                 return true;
             }
