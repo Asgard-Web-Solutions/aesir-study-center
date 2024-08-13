acc7d323055a6fb269b0e06cc6ec65d4622472c9
Shift <shift@laravelshift.com>
2024-08-13 19:00:16 +0000

    Shift cleanup

--- config/app.php
+++ config/app.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
 
 return [
-
 
     'aliases' => Facade::defaultAliases()->merge([
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -16,6 +15,7 @@ class UserFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password;
+
     /**
      * Define the model's default state.
      *
