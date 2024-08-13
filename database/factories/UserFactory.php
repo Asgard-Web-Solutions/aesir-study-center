0a658ba4a0742e28baec34f0398a0eb3a54b1be6
Shift <shift@laravelshift.com>
2024-08-13 18:59:34 +0000

    Remove unnecessary `$model` property

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,9 +16,6 @@ class UserFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password;
-
-    protected $model = User::class;
-
     /**
      * Define the model's default state.
      *
