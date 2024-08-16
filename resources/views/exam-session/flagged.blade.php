eed0c6bf1213444917445bc6b4efbb52e69596d6
Jon Zenor <JonZenor@gmail.com>
2024-08-16 00:43:45 -0600

    Updated the Review Book toggle page during exam sessions

--- resources/views/exam-session/flagged.blade.php
+++ resources/views/exam-session/flagged.blade.php
@@ -6,11 +6,16 @@
 
     <x-card.main title='Review Book Updated'>
         <x-card.mini>
-            This question was added to your review book.
+            @if ($userQuestion->reviewFlagged)
+                This question was Added to your Review Book.
+            @else
+                This question was Removed from your Review Book.
+            @endif
         </x-card.mini>
 
         <x-card.mini>
             <div class="block w-full text-right lg:flex">
+                <a href="{{ route('exam-session.toggleReviewFlag', ['set' => $exam, 'question' => $question]) }}" class="btn"><i class="{{ config('icon.review_flag') }} @if ($userQuestion->reviewFlagged) text-{{ config('color.review_flag_on') }} @else text-{{ config('color.review_flag_off') }} @endif text-3xl"></i></a>
                 <a href="{{ route('practice.start', $exam) }}" class="m-2 btn btn-secondary btn-outline">Start Practice Session</a>
                 <a href="{{ route('exam-session.test', $exam) }}" class="m-2 btn btn-primary">Next Question</a>
             </div>
