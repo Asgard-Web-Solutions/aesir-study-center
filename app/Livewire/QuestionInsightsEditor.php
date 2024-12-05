3e53bb615215f38e186ee7707396da4898cb7840
Jon Zenor <JonZenor@gmail.com>
2024-12-04 22:46:03 -0700

    Moved insight save functionality to an Action

--- app/Livewire/QuestionInsightsEditor.php
+++ app/Livewire/QuestionInsightsEditor.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Actions\MasteryInsights\RecordInsightResponse;
 use App\Models\Insight;
 use App\Models\Question;
 use Flux\Flux;
@@ -25,16 +26,7 @@ class QuestionInsightsEditor extends Component
     public function save() {
         $this->validate();
 
-        if ($this->insight == null) {
-            $this->insight = new Insight();
-            $this->insight->ai_generated = 0;
-        }
-
-        $this->insight->insight_text = $this->insight_text;
-        $this->insight->question_id = $this->question->id;
-        $this->insight->user_id = auth()->id();
-
-        $this->insight->save();
+        $this->insight = RecordInsightResponse::execute($this->question, 0, $this->insight_text);
 
         Flux::toast('Mastery Insight Saved');
         $this->modal('edit-insight')->close();
