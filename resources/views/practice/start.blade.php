1e0c1a6979d3c3f2b494371f4e1b2dfa5b75066c
Jon Zenor <JonZenor@gmail.com>
2024-12-20 12:36:34 -0700

    Updated the practice select box to be a radio select instead of dropdown

--- resources/views/practice/start.blade.php
+++ resources/views/practice/start.blade.php
@@ -10,7 +10,17 @@
                 @php
                     $options = [ 'all' => "All Questions", 'recentIncorrect' => 'Recently Incorrect Answers', 'flagged' => "Review Book", 'weak' => "Weak/Low Mastery", 'strong' => "Strong/High Mastery"];
                 @endphp
-                <x-form.dropdown :values=$options label="Select Review Set" name="filter" selected='recentIncorrect' />
+
+                <div class="my-4 space-y-4">
+                    @foreach ($options as $key => $option)
+                        <div class="flex items-center">
+                            <input type="radio" id="{{ $key }}" class="max-w-lg mx-2 text-primary-content radio radio-primary" name="filter" value="{{ $key }}" @if ($key == "recentIncorrect") checked @endif>
+                            <label class="label" for="{{ $key }}">
+                                <span class="text-primary">{{ $option }}</span>
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
 
                 <x-card.buttons submitLabel="Start Practice Session" />
             </form>
