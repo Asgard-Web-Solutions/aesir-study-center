1c3415ad62d24b602544277a884a12b8f0819169
Shift <shift@laravelshift.com>
2026-02-11 04:55:26 +0000

    Remove base controller inheritance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class Controller
 {
 
     public function getAuthedUser()
