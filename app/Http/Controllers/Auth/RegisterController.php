52c2efd6c5f2215046a6b840ca9f2c91a258ceb6
Shift <shift@laravelshift.com>
2026-02-11 04:55:26 +0000

    Modernize controller middleware

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\AppServiceProvider;
@@ -12,7 +14,7 @@ use Rahul900day\Captcha\Rules\Captcha as CaptchaRule;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Pennant\Feature;
 
-class RegisterController extends Controller
+class RegisterController extends Controller implements HasMiddleware
 {
     /*
     |--------------------------------------------------------------------------
@@ -34,14 +36,11 @@ class RegisterController extends Controller
      */
     protected $redirectTo = AppServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public static function middleware(): array
     {
-        $this->middleware('guest');
+        return [
+            'guest',
+        ];
     }
 
     /**
